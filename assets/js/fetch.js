026613bed8220869ca41d3ef069891b1ee4f7a99
Elisa Bothy <elisa.bothy@gmail.com>
2024-01-12 16:05:09 +0100

    la photo de la nasa du jour

---
+++ assets/js/fetch.js
@@ -0,0 +1,19 @@
+const uri = "https://api.nasa.gov/planetary/apod?api_key=79Nr41pzNDhCwyeNkiOw2mIDcJSi2scyEPAmkcav "
+fetch(uri)
+        .then(resp => {
+            if (resp.ok){
+                return resp.json();
+            }
+            else{
+                throw new Error("Données non collectées");
+            }
+        })
+        .then(obj => {
+            const img = document.querySelector("img");
+            const urlImg = obj.url;
+            img.alt = "Image du jour de la Nasa : " + obj.date;
+            img.src = urlImg;
+        })
+        .catch(err => {
+            console.error(err);
+        })
